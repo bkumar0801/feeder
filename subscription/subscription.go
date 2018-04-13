d8c918187c748a73c8228a4af5fa0bdeb2aa6f2e
Bijendra Kumar <kumarbijendra@gmail.com>
2018-04-13 12:27:47 +0200

    Bijendra | Improve fetch duplicates and make fetch call asynchronosu

--- subscription/subscription.go
+++ subscription/subscription.go
@@ -12,7 +12,8 @@ type Item struct {
 }
 
 /*
-Subscription ...
+Subscription ...creates streams of items
+shuts down the stream
 */
 type Subscription interface {
 	Updates() <-chan Item // stream of Items
@@ -20,7 +21,7 @@ type Subscription interface {
 }
 
 /*
-Fetcher ...
+Fetcher ...fetches items from web
 */
 type Fetcher interface {
 	Fetch() (items []Item, next time.Time, err error)
@@ -37,6 +38,9 @@ func (s *sub) Updates() <-chan Item {
 	return s.updates
 }
 
+/*
+Close ...asks loop to exit and waits for a response.
+*/
 func (s *sub) Close() error {
 	errc := make(chan error)
 	s.closing <- errc
@@ -120,15 +124,26 @@ func Merge(subs ...Subscription) Subscription {
 // loop fetches items using s.fetcher and sends them
 // on s.updates.  loop exits when s.Close is called.
 func (s *sub) loop() {
-	var pending []Item // appended by fetch; consumed by send
-	var next time.Time // initially January 1, year 0
+	const maxPending = 10 // less than 10% pending items are allowed
+	type fetchResult struct {
+		fetched []Item
+		next    time.Time
+		err     error
+	}
+	var fetchDone chan fetchResult // if non-nil, Fetch is running
+	var pending []Item             // appended by fetch; consumed by send
+	var next time.Time             // initially January 1, year 0
 	var err error
+	var seen = make(map[string]bool) // set of item.GUIDs to remove duplicates
 	for {
 		var fetchDelay time.Duration // initally 0 (no delay)
 		if now := time.Now(); next.After(now) {
 			fetchDelay = next.Sub(now)
 		}
-		startFetch := time.After(fetchDelay)
+		var startFetch <-chan time.Time
+		if fetchDone == nil && len(pending) < maxPending { //fetch asynchronously when there is less items pending
+			startFetch = time.After(fetchDelay) // enable fetch case
+		}
 
 		var first Item
 		var updates chan Item
@@ -138,18 +153,31 @@ func (s *sub) loop() {
 		}
 
 		select {
-		case errc := <-s.closing:
+		case errc := <-s.closing: //reply with the Fetch error and exit
 			errc <- err
 			close(s.updates) // tells receiver we're done
 			return
-		case <-startFetch:
-			var fetched []Item
-			fetched, next, err = s.fetcher.Fetch()
+		case <-startFetch: // start fetch when there are less tha 10% items in pending and when fetch completed
+			fetchDone = make(chan fetchResult, 1)  //channel to make fetch asynchronous
+			go func() {
+				fetched, next, err := s.fetcher.Fetch()
+				fetchDone <- fetchResult{fetched, next, err}
+			}()
+		case result := <-fetchDone:
+			fetchDone = nil
+			// Use result.fetched, result.next, result.err
+			fetched := result.fetched
+			next, err = result.next, result.err
 			if err != nil {
 				next = time.Now().Add(10 * time.Second)
 				break
 			}
-			pending = append(pending, fetched...)
+			for _, item := range fetched {
+				if id := item.GUID; !seen[id] {
+					pending = append(pending, item)
+					seen[id] = true
+				}
+			}
 
 		case updates <- first:
 			pending = pending[1:]
